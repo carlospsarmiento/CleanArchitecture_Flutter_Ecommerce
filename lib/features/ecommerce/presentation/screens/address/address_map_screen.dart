2337e94fa3962ddd83eff09db2b771b328188c84
Luis Rojas <luis.rojas@accenti.io>
2024-12-10 17:18:45 -0500

    advance of maps

--- lib/features/ecommerce/presentation/screens/address/address_map_screen.dart
+++ lib/features/ecommerce/presentation/screens/address/address_map_screen.dart
@@ -1,13 +1,35 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
-class AddressMapScreen extends StatelessWidget {
+class AddressMapScreen extends StatefulWidget {
   const AddressMapScreen({super.key});
+
+  @override
+  State<AddressMapScreen> createState() => _AddressMapScreenState();
+}
+
+class _AddressMapScreenState extends State<AddressMapScreen> {
+
+  final Completer<GoogleMapController> _controller = Completer<GoogleMapController>();
+
+  static const CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(-8.1117802, -79.0311619),
+    zoom: 14.4746,
+  );
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Elija su dirección en el mapa"),
+      ),
+      body: GoogleMap(
+        mapType: MapType.normal,
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
       ),
     );
   }
